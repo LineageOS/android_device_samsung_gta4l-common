d386db5758e2f5b6c420c543abf832ee09474aaf
Daniel Rosenberg <drosen@google.com>
2021-07-16 12:52:43 +0200

    gta4l-common: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c

--- gta4l.mk
+++ gta4l.mk
@@ -204,6 +204,9 @@ PRODUCT_PACKAGES += \
     vendor_firmware-modem_mountpoint \
     vendor_firmware_mnt_mountpoint
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
