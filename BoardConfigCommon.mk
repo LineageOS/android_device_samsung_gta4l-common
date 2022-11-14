4efbb555faa9f9588904f2de493580bdcdd7d2ec
Michael Bestas <mkbestas@lineageos.org>
2022-11-14 13:11:09 +0200

    gta4l-common: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,6 +98,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/samsung/sm6115
 TARGET_KERNEL_CONFIG := \
     vendor/bengal-perf_defconfig \
