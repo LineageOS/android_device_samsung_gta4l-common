3b717a8c7f26ff3f6ad4ed6262c552c99ab16aca
Bruno Martins <bgcngm@gmail.com>
2023-10-24 00:51:18 +0100

    gta4l-common: Also patch gatekeeper HAL to load libcrypto-v33.so
    Then again, due to asn1t.h structs changes.
    
    Change-Id: Id3552124ddc07bf80446fd2b37b40f39c6be7a76

--- extract-files.sh
+++ extract-files.sh
@@ -29,7 +29,7 @@ function blob_fixup() {
         vendor/lib64/libgui_vendor.so)
             "${PATCHELF}" --replace-needed "libui.so" "libui-v30.so" "${2}"
             ;;
-        vendor/lib64/libskeymaster4device.so)
+        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
 	vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
