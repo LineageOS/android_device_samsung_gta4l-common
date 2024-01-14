0fbecd545395097f7c9f5a196188cf16ac6b224e
Christian Hoffmann <chrmhoffmann@gmail.com>
2024-01-14 18:13:31 +0100

    gta4l-common: Build camera libs from source * Heavily inspired by gts4lv-commmon/camera
    Change-Id: I31770e20b524676f560c56f51e4a94175ff29f86

--- gta4l.mk
+++ gta4l.mk
@@ -102,7 +102,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.common@2.0.vendor \
     android.frameworks.cameraservice.device@2.0.vendor \
-    android.frameworks.cameraservice.service@2.1.vendor
+    android.frameworks.cameraservice.service@2.1.vendor \
+    android.hardware.camera.provider@2.4-impl.gta4l \
+    android.hardware.camera.provider@2.4-service_64.gta4l
 
 # Charger
 PRODUCT_PACKAGES += \
