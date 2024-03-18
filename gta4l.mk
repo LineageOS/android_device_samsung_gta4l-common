8b557f662606013d360a144e86f6dc56b685b282
LuK1337 <priv.luk@gmail.com>
2024-03-18 18:18:12 +0100

    gta4l-common: Use libcrypto-v33 from hardware/lineage/compat
    Because vndk prebuilt has SONAME set to libcrypto.so, it prevents linker
    from loading libcrypto.so built from source.
    
    Change-Id: Ic9e29f7b00b469163f48ea407fc9b5d5e90816c8

--- gta4l.mk
+++ gta4l.mk
@@ -324,8 +324,10 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libui-v30.so \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/android.hardware.common-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.common-V1-ndk_platform.so \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/android.hardware.graphics.common-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.graphics.common-V1-ndk_platform.so \
-    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v33.so
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
+
+PRODUCT_PACKAGES += \
+    libcrypto-v33
 
 # WiFi
 PRODUCT_COPY_FILES += \
