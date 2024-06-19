af8f8427a04f37eff524ecd100f62256aab1a23e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 12:28:11 +0800

    gta4l-common: Drop SoundTrigger
    * Never ensured it's working in before (gta4l never had hal)
    * ST 2.1 interface is deprecated on vintf target-level 6
    
    Change-Id: I189e0dca38f74be0343c5b345481f3bb7c95bc65
    (cherry picked from commit 8b0c6a2222f88f9ad255da3bf64fffd88dd944d4)

--- gta4l.mk
+++ gta4l.mk
@@ -56,7 +56,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl.gta4l:32 \
     android.hardware.bluetooth.audio-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
